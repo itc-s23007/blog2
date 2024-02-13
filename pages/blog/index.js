a3b9b2e7749dcd6a3aff181d918d3679e982acdb
s23007 <s23007@std.it-college.ac.jp>
2024-02-13 05:38:06 +0000

    9.6

---
+++ pages/blog/index.js
@@ -0,0 +1,42 @@
+import { getAllPosts } from 'lib/api'
+import Meta from 'components/meta'
+import Container from 'components/container'
+import Hero from 'components/hero'
+import Posts from 'components/posts'
+import { getImageBuffer } from 'lib/getImageBuffer'
+import { getPlaiceholder } from 'plaiceholder'
+
+import { eyecatchLocal } from 'lib/constants'
+const Blog = ({ posts }) => {
+  return (
+    <Container>
+      <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
+      <Hero title='Blog' subtitle='Recent Posts' />
+
+      <Posts posts={posts} />
+    </Container>
+  )
+}
+
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+    console.log('base64', base64)
+  }
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
+
+export default Blog
+export { getStaticProps }
